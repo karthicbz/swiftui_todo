4f5c20c7f46d6598eb89a707f82a910a3c0aa52d
Karthicbz <pkarthicbz@live.in>
2025-12-03 23:07:54 +0530

    add strikethrough on completed task and show delete option

---
+++ Todo List/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Todo List
+//
+//  Created by Karthick P on 03/12/25.
+//
+
+import SwiftUI
+
+struct TodoList: Identifiable {
+    let todo: String
+    var isCompleted: Bool
+    let id: UUID
+}
+
+struct ContentView: View {
+    @State private var todoText: String = ""
+    @State private var todos: [TodoList] = []
+    
+    func deleteTodo(todo:TodoList){
+        let newTodoList = todos.filter{todoItem in
+            todo.id != todoItem.id
+            
+        }
+        todos = newTodoList
+    }
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                TextField("Enter a task..", text: $todoText)
+                Button("Add") {
+                    todos.append(
+                        TodoList(todo: todoText, isCompleted: false, id: UUID())
+                    )
+                    todoText = ""
+                }
+                .buttonStyle(.borderedProminent)
+            }.frame(height: 50)
+            VStack(alignment: .leading) {
+                ForEach($todos) { $todo in
+                    VStack(alignment: .leading) {
+                        TodoItem(todo: $todo, deleteTodo: deleteTodo)
+                        Divider()
+                    }
+                }
+            }
+            Spacer()
+        }
+        .navigationTitle("Todo List")
+        .padding([.leading, .trailing, .top], 16)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo List/views/TodoItem.swift
@@ -0,0 +1,44 @@
+//
+//  TodoItem.swift
+//  Todo List
+//
+//  Created by Karthick P on 04/12/25.
+//
+
+import SwiftUI
+
+struct TodoItem: View {
+    @Binding var todo:TodoList
+//    let id:UUID
+//    @State var todos:[TodoList]
+    var deleteTodo: (_ todo:TodoList)->Void
+    var body: some View {
+        Button(
+            action: {
+                todo.isCompleted = !todo.isCompleted
+            },
+            label: {
+                HStack {
+                    Rectangle()
+                        .fill((todo.isCompleted) ? .green : .white)
+                        .frame(width: 20, height: 20)
+                        .border(.black, width: 1)
+                    Text(todo.todo)
+                        .font(.title3)
+                        .multilineTextAlignment(.leading)
+                        .strikethrough(todo.isCompleted, color: .black)
+                    Spacer()
+                    Button("Delete")
+                    {
+                        deleteTodo(todo)
+                    }.tint(.red)
+                }
+            }
+        )
+    }
+}
+
+#Preview {
+    TodoItem(todo: .constant(TodoList(todo: "Water Rhino", isCompleted: false, id: UUID()))) { todo in
+    }
+}
